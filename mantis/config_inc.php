0561b701f59a571742c5cafad85b659ca5289aea
in.vaibhavjain@gmail.com <in.vaibhavjain@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-05-05 14:12:18 +0000

    added changes by Ashish Gupta

--- mantis/config_inc.php
+++ mantis/config_inc.php
@@ -3,5 +3,6 @@
 	$g_db_type = 'mysql';
 	$g_database_name = 'bugtracker';
 	$g_db_username = 'root';
-	$g_db_password = '';
+	$g_db_password = 'theshoppingpro@123';
+date_default_timezone_set('Asia/Kolkata');
 ?>
