3bf0af7fa0a8a62b4260feb4a0ab08e866f89af3
in.vaibhavjain@gmail.com <in.vaibhavjain@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-06-03 09:30:35 +0000

    Changes to page--welcome_message.tpl.php

--- sites/all/themes/ocarina/templates/page--welcome_message.tpl.php
+++ sites/all/themes/ocarina/templates/page--welcome_message.tpl.php
@@ -1,17 +1,16 @@
 <div style="text-align:center;" id="welcome-message">
   <div>
-    <img src="sites/default/files/The_Shopping_Pro_Logo-250px.png" />
+    <img src="sites/default/files/The_Shopping_Pro_Logo-250px.png" style="width: 300px;" />
   </div>
   <div style="color:#25ade3;">
-    Easy, Intelligent & Personalized Shopping
+    Easy, Intelligent & Regarding Shopping
   </div>
   <div>
     <?php
-    print l('<img src="sites/all/themes/ocarina/images/Play_Video.png" />', 'http://www.youtube.com/embed/-iTMCF4gCgA',
-      array(
+    print l('<img src="sites/all/themes/ocarina/images/Play_Video.png" />', 'http://www.youtube.com/embed/-iTMCF4gCgA', array(
         'external' => TRUE,
-        'query' => array('width' => '560', 'height' => '315', 'iframe' => 'true'),
-        'html' =>TRUE,
+        'query' => array('width' => '640', 'height' => '400', 'iframe' => 'true'),
+        'html' => TRUE,
         'attributes' => array('class' => 'colorbox-load'))
       );
     ?>
@@ -19,30 +18,38 @@
   <div>
     <?php
     if (is_chrome()) {
-      print '<button onclick="chrome.webstore.install()" style="background-image: url(sites/all/themes/ocarina/images/blue/blue_button_chrome.png);height: 70px;width: 100%;background-size: 100%;background-repeat: no-repeat;border: none;" id="install-button"> </button>';
-    } else {
+      print '<button onclick="chrome.webstore.install()"style="background-image: url(http://theshoppingpro.com/sites/all/themes/ocarina/images/blue/blue_button_big_chrome.png);height: 90px;width: 60%;background-size: 100%;background-repeat: no-repeat;border: none;" id="install-button"> </button>';
+      ?>
+      <div style="color:#ffffff">Having Trouble? Use <a href="http://crossrider.com/download/28108">Alternate Link</a></div>
+<!--      <div style="color:#ffffff"><p>Available for all your favorite browsers:</p></div><p><img alt="" src="http://www.theshoppingpro.com/sites/all/themes/ocarina/images/browser-logos-small.png"></p><div style="color:#FFFFFF"><p>Coming soon to your mobile devices.</p></div>-->
+      <?php
+    }
+    else {
       ?>
       <script type="text/javascript" src="https://w9u6a2p6.ssl.hwcdn.net/javascripts/installer/installer.js"></script>
       <script type="text/javascript">
         var __CRI = new crossriderInstaller({
-          app_id: 28108,
-          app_name: 'TheShoppingPro'
+          app_id:28108,
+          app_name:'TheShoppingPro'
         });
 
         var _cr_button = new __CRI.button({
-          text: 'Click here to install The Shop Mate plugin',
-          button_size: 'medium',
-          color: 'blue'
+          text:'Click here to Install The Shop Mate Plugin',
+          button_size:'big',
+          color:'blue'
         });
-      //__CRI.install(); //use this if you want to use your own button
+
+        //__CRI.install(); //use this if you want to use your own button
       </script>
       <div id="crossriderInstallButton"></div>
+      <div style="color:#ffffff">Having Trouble? Use <a href="http://crossrider.com/download/28108">Alternate Link</a></div>
+<!--      <div style="color:#ffffff"><p>Available for all your favorite browsers:</p></div><p><img alt="" src="http://www.theshoppingpro.com//sites/all/themes/ocarina/images/browser-logos-small.png"></p><div style="color:#FFFFFF"><p>Coming soon to your mobile devices.</p></div>-->
       <?php
     }
     ?>
   </div>
   <div>
-    Free for <img src="sites/all/themes/ocarina/images/OS_Logo.png" /> on <img src="sites/all/themes/ocarina/images/browser-logos-small.png" />
+    Free for <img src="sites/all/themes/ocarina/images/browser-logos-small.png" /> on <img src="sites/all/themes/ocarina/images/OS_Logo.png" style="height: 37px !important;" />
   </div>
 </div>
 <div id="video" style="display: none;">
