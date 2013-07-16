23b2e299cb0406ed22dce4fec75529797c3035fc
bendrupal@gmail.com <bendrupal@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-07-16 15:32:13 +0000

    update

--- sites/all/modules/partner_program/partner-program-script-page.tpl.php
+++ sites/all/modules/partner_program/partner-program-script-page.tpl.php
@@ -9,13 +9,32 @@
 	</thead>
 	<tr>
 	<td>
-		<textarea class="partner_program"  wrap='off' autocorrect='off' autocapitalize='off' tabindex='0' rows="6" cols="20">
+		<textarea class="partner_program" id="box-content"  name="box-content"   wrap='off' autocorrect='off' autocapitalize='off' tabindex='0' rows="6" cols="20">
 <!--script start here-->		
 <script type="text/javascript">var referralid='<?php print $data['referrerid'] ?>';</script>
 <script type="text/javascript" src="http://www.theshoppingpro.com/sites/all/modules/partner_program/shoppingpro.js">
 </script>
 <!--script end here-->		
 		</textarea>
+                
+<br /><br />
+<p><input type="button" id="copy" name="copy" value="Copy to Clipboard" /></p>	
+<script>		
+		//set path
+ZeroClipboard.setMoviePath('http://davidwalsh.name/demo/ZeroClipboard.swf');
+//create client
+var clip = new ZeroClipboard.Client();
+//event
+clip.addEventListener('mousedown',function() {
+	clip.setText(document.getElementById('box-content').value);
+});
+clip.addEventListener('complete',function(client,text) {
+	alert('copied: ' + text);
+});
+//glue it to the button
+clip.glue('copy');
+</script>		
+		        
 
 	</td>
 	</tr>
