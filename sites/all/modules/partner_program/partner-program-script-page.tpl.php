1b0dea5e222e94070f15c19ec139fca2998d8532
bendrupal@gmail.com <bendrupal@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-07-16 12:25:21 +0000

    Partner Program Script Custom module

---
+++ sites/all/modules/partner_program/partner-program-script-page.tpl.php
@@ -0,0 +1,26 @@
+<div id="id_referrer_content_plugin" class="referrer_content">
+  <div id="referrer_content" class="plugin_wrapper"> 
+  &nbsp;
+  	<table cell-padding=10>
+	<thead>
+	<tr>
+		<th>Partner Program Script</th>
+	</tr>
+	</thead>
+	<tr>
+	<td>
+		<textarea class="partner_program"  wrap='off' autocorrect='off' autocapitalize='off' tabindex='0' rows="6" cols="20">
+<!--script start here-->		
+<script type="text/javascript">var referralid='<?php print $data['referrerid'] ?>';</script>
+<script type="text/javascript" src="http://www.theshoppingpro.com/sites/all/modules/custom/partner_program/shoppingpro.js">
+</script>
+<!--script end here-->		
+		</textarea>
+
+	</td>
+	</tr>
+	</table>
+  </div>
+</div>
+
+
---
+++ sites/all/modules/partner_program/shoppingpro.js
@@ -0,0 +1,8 @@
+/* Generate a script for Partner Program */
+
+var referral_url = 'http://www.theshoppingpro.com/referral';
+document.write("<a href='"+referral_url+"/"+referralid+"'><img src='http://theshoppingpro.com/sites/all/themes/ocarina/images/TheShoppingPro-Emblem.png' width='75px' height='75px'/></a>");
+
+
+
+
