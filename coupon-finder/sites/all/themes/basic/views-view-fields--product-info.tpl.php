c34a21b8659ac5138a63d1dca899af946419ec60
sk79293@gmail.com <sk79293@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-11-22 09:44:45 +0000

    add

--- coupon-finder/sites/all/themes/basic/views-view-fields--product-info.tpl.php
+++ coupon-finder/sites/all/themes/basic/views-view-fields--product-info.tpl.php
@@ -33,12 +33,12 @@ $nid = arg(1);
 <div class="product-left">
     <?php
     
-    echo "<a target='_blank' class='view_store' href='{$node->field_affiliateurl[und][0]['value']}'>".$fields['field_product_image']->content."</a>";
+    echo "<a target='_blank' class='view_store' href='{$fields['field_affiliateurl']->content}'>".$fields['field_product_image']->content."</a>";
         
     //print ($fields['field_product_images']->content);
     $status = strip_tags($fields['field_best_coupon_status']->content);
     if( $status == 1){
-        echo "<div class='d_view_store'><a target='_blank' class='view_store' href='{$node->field_affiliateurl[und][0]['value']}'>View Store</a></div>";
+        echo "<div class='d_view_store'><a target='_blank' class='view_store' href='{$fields['field_affiliateurl']->content}'>View Store</a></div>";
     }
     //field_base_url
     //nid
