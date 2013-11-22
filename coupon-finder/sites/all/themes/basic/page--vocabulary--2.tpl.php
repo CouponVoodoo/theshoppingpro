0e3d590f42f0d33519cb7c279653fc5cac0b2261
sk79293@gmail.com <sk79293@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-11-22 12:04:01 +0000

    add

--- coupon-finder/sites/all/themes/basic/page--vocabulary--2.tpl.php
+++ coupon-finder/sites/all/themes/basic/page--vocabulary--2.tpl.php
@@ -3,8 +3,7 @@
     <div class="ribbon-wrapper-green">
         <div class="ribbon-green">Save <?php echo $node->field_best_coupon_saving['und'][0]['value']; ?></div>
   </div>
-  <?php }?> 
-  
+  <?php }?>
   <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
     <header>
       <?php print render($title_prefix); ?>
@@ -32,7 +31,7 @@
       hide($content['comments']);
       hide($content['links']);
       
-      
+      //echo "<div class='product_name'><a href='{$node->field_page_url[und][0]['value']}' >".substr($node->field_retailer_product_name[und][0]['value'], 0, 47)."</a></div>";
       
       if( $node->field_best_coupon_status[und][0]['value'] == 1 ){
         echo "<div class='coupons_found'><img src='".base_path().path_to_theme()."/images/u67_normal.png' /><div class='coupons_text'>Coupons Found</div></div>";
@@ -45,22 +44,21 @@
       
       $uplImg = $node->field_product_images['und'][0]['uri'];
       $imgPath = $imgUri = image_style_url('200x200', $uplImg);
-      $img = "<div class='field field-name-field-product-images field-type-image field-label-above'>
+      $img = "<div class='field field-name-field-product-images field-type-image field-label-above'>    
                     <div class='field-label'>Product images:&nbsp;</div>
                     <div class='field-items'>
                         <div class='field-item even product_img'>
                             <a target='_blank'  href='{$urlAlias}'><img src='{$node->field_product_image['und'][0]['value']}' typeof='foaf:Image'></a>
                         </div>";
-                     
       echo  $img .=    "</div>
                 </div>";
       
       echo "<div class='product_name'><a target='_blank'  href='{$urlAlias}' >".substr($node->field_retailer_product_name[und][0]['value'], 0, 47)."</a></div>";
-                
+      
       echo $listPrice = "<div class='field field-name-field-product-price field-type-number-integer field-label-above'>
                         <div class='field-label'>List Price:&nbsp;</div>
                         <div class='field-items'>
-                            <div class='field-item even'>INR ".number_format( $node->field_product_price['und'][0]['value'],0, '.', ',')."</div>
+                            <div class='field-item even'>INR ".number_format($node->field_product_price['und'][0]['value'],0, '.', ',')."</div>
                         </div>
                     </div>";
       $listPrice = "<div class='field field-name-field-best-coupon-saving field-type-number-integer field-label-above'>
@@ -71,7 +69,7 @@
             $listPrice .=          '-';
                                 }
                                 else{
-            $listPrice .=        'INR '. number_format($node->field_best_coupon_saving['und'][0]['value'],0, '.', ',');
+            $listPrice .=          'INR '. number_format($node->field_best_coupon_saving['und'][0]['value'],0, '.', ',');
                                 }    
       echo $listPrice .=         "</div>
                             </div>
@@ -79,7 +77,7 @@
       echo $listPrice = "<div class='field field-name-field-best-coupon-netpriceafters field-type-number-integer field-label-above'>
                             <div class='field-label'>Net Price:&nbsp;</div>
                             <div class='field-items'>
-                                <div class='field-item even'>INR ".number_format( $node->field_best_coupon_netpriceafters['und'][0]['value'],0, '.', ',')."</div>
+                                <div class='field-item even'>INR ".number_format($node->field_best_coupon_netpriceafters['und'][0]['value'],0, '.', ',')."</div>
                             </div>
                         </div>";       
       //print render($content);
