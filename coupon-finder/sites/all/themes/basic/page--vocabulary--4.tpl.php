4dad2d8e148cfa00ac3dd1f73bcc80e8a9f17217
sk79293@gmail.com <sk79293@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-11-25 06:07:44 +0000

    update

--- coupon-finder/sites/all/themes/basic/page--vocabulary--4.tpl.php
+++ coupon-finder/sites/all/themes/basic/page--vocabulary--4.tpl.php
@@ -98,12 +98,13 @@
       
       //echo 'brand - status '. $node->field_best_coupon_status[und][0]['value'];
       if($node->field_best_coupon_status[und][0]['value'] == 1){
-        $copyCoupon =  coupons_copy_coupon_taxonomy($node->nid,4);
-        print render($copyCoupon);
+        //$copyCoupon =  coupons_copy_coupon_taxonomy($node->nid,4);
+        //print render($copyCoupon);
       }
-      if($node->field_best_coupon_status[und][0]['value'] == 0){
-        echo "<div class='d_view_store'><a target='_blank' class='view_store' href='{$node->field_affiliateurl[und][0]['value']}'>View Store</a></div>";
-      }      
+      //if($node->field_best_coupon_status[und][0]['value'] == 0){
+        //echo "<div class='d_view_store'><a target='_blank' class='view_store' href='{$node->field_affiliateurl[und][0]['value']}'>View Store</a></div>";
+        echo "<div class='d_view_store'><a target='_blank' class='view_store' href='{$node->field_page_url[und][0]['value']}'>View Store</a></div>";
+      //}           
       //$field_page_url = $node->field_page_url[und][0]['value'];
       //echo "<div class='page_url'><a href='$field_page_url'>See Other Coupons</a></div>";
      ?>
