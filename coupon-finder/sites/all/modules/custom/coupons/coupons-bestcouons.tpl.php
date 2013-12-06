f9cad9b87f05e51b25c07b47b852acebc25babbf
sk79293@gmail.com <sk79293@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2013-12-06 10:03:29 +0000

    add

--- coupon-finder/sites/all/modules/custom/coupons/coupons-bestcouons.tpl.php
+++ coupon-finder/sites/all/modules/custom/coupons/coupons-bestcouons.tpl.php
@@ -11,11 +11,11 @@ global $base_url;
 <div class="search_listing_right">
   <div class="search_listing_row__<?php print $row->counter; ?> copy_coupon_row">
     <?php if ($row->url): ?>
-    <a href="<?php print $base_url ?>/coupon-redirect?s=<?php print urlencode($row->url); ?>" target="_blank" class="unlock_coupon" rel="best_<?php print $row->counter; ?>">
+    <a href="<?php print $base_url ?>/coupon-redirect?s=<?php print urlencode($row->url); ?>" target="_blank" class="unlock_best_coupon unlock_coupon" rel="best_<?php print $row->counter; ?>">
       <span class="copy_coupon">Copy Coupon</span><span></span>
     </a>
     <?php else : ?>
-    <a href="javascript:" class="unlock_coupon" rel="best_<?php print $row->counter; ?>">
+    <a href="javascript:" class="unlock_best_coupon unlock_coupon" rel="best_<?php print $row->counter; ?>">
       <span class="copy_coupon">Copy Coupon</span><span></span>
     </a>
     <?php endif; ?>
