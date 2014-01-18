6b37ebf0870fd53e4edc89afd41d88f71448e8cd
lavesh626@gmail.com <lavesh626@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-01-18 11:42:35 +0000

    visible copy_coupon_taxo.tpl.php

--- coupon-finder/sites/all/modules/custom/coupons/copy_coupon_taxo.tpl.php
+++ coupon-finder/sites/all/modules/custom/coupons/copy_coupon_taxo.tpl.php
@@ -7,12 +7,12 @@ if ($row->Successful=="1") {
   $image_right = $base_url. "/". drupal_get_path('theme', 'basic')."/images/u71_normal.png";
   $best_coupon = !empty($row->BestCoupon) ? '<span class="best_coupon">Best Coupon</span>' : '<span class="best_coupon_image red">This coupon does not work for your product</span>';
 }
-?>
+?>couponCode
 <div class="search_listing_right2">
   <div class="search_listing_row__<?php print $row->counter; ?> copy_coupon_row">
     <?php if ($row->url): ?>
     <a href="<?php print $base_url ?>/coupon-redirect?s=<?php print urlencode($row->url); ?>" target="_blank" class="unlock_coupon" rel="c_<?php print $row->counter; ?>">
-      <span class="copy_coupon">Copy Coupon</span><span></span>
+      <span class="copy_coupon"><?php print $row->couponCode</span><span></span>
     </a>
     <?php else : ?>
     <a href="javascript:" class="unlock_coupon" rel="c_<?php print $row->counter; ?>">
