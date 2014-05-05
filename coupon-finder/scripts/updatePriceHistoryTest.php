dba0a20c94c6cee9b1114b44a5235fdcf5f1b156
lavesh626@gmail.com <lavesh626@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-05-05 05:44:13 +0000

    priceHistory Update test

---
+++ coupon-finder/scripts/updatePriceHistoryTest.php
@@ -0,0 +1,20 @@
+<?PHP
+
+
+
+$results = db_query("SELECT distinct entity_id from coupon_finder.priceHistory where updateDate = CURDATE()");
+
+foreach ($results as $result) {
+	echo 'inside loop';
+	echo $result->entity_id;
+	
+	$entity_id = explode(",",$result->entity_id);
+
+            $query = "update coupon_finder.predictorCompiledResultTable set updatePriceHistoryStatus =1 where entity_id = :entity_id";
+			$count = db_query($query, array(':entity_id' => $entity_id));
+	}
+	
+db_query("Insert into priceHistory select '',entity_id,BestCouponStatus,NetPrice,ListPrice,CURDATE(),0 from predictorCompiledResultTable where updatePriceHistoryStatus <> 1")	;
+db_query("update coupon_finder.predictorCompiledResultTable set updatePriceHistoryStatus =0")	;
+
+?>
