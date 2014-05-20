3fbb40a0b316dcffe1a6de59bd6dabd4fcd89a0c
lavesh626@gmail.com <lavesh626@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-05-20 09:52:38 +0000

    priceHistory Update test

--- coupon-finder/scripts/updatePriceHistoryTest.php
+++ coupon-finder/scripts/updatePriceHistoryTest.php
@@ -1,8 +1,18 @@
 <?PHP
 
 
+$freq=15;
 
+$result = db_query('SELECT status FROM coupon_finder.1Variables n where Serial=3');
+foreach(
+$result as $item) {
+  $updateCount= $item->status;
+}
+	if ($updateCount < $freq){echo 'inside';}
+/*
 $results = db_query("SELECT distinct entity_id from coupon_finder.priceHistory where updateDate = CURDATE()");
+
+
 
 foreach ($results as $result) {
 	echo 'inside loop';
@@ -12,10 +22,27 @@ foreach ($results as $result) {
 
             $query = "update coupon_finder.predictorCompiledResultTable set updatePriceHistoryStatus =1 where entity_id = :entity_id";
 			$count = db_query($query, array(':entity_id' => $entity_id));
+			//$update=1;
 	}
 	
+	$result = db_query('SELECT status FROM coupon_finder.1Variables n where Serial=3');
+foreach(
+$result as $item) {
+  $updateCount= $item->status;
+}
+	if ($updateCount < $freq){
 $count=db_query("Insert into priceHistory select 0,entity_id,BestCouponStatus,NetPrice,ListPrice,CURDATE(),0,BestCouponCode from predictorCompiledResultTable where updatePriceHistoryStatus <> 1")	;
+$count=db_query("Insert into priceHistoryBatchData select 0,entity_id,BestCouponStatus,NetPrice,ListPrice,CURDATE(),0,BestCouponCode from predictorCompiledResultTable where updatePriceHistoryStatus <> 1")	;
+
 echo $count;
 db_query("update coupon_finder.predictorCompiledResultTable set updatePriceHistoryStatus =0")	;
-
+db_query("update coupon_finder.1Variables set status = status+1 where Serial=3")	;
+}
+else {
+db_query("Truncate table priceHistoryBatchData")	;
+db_query("Insert into priceHistory select 0,entity_id,BestCouponStatus,NetPrice,ListPrice,CURDATE(),0,BestCouponCode from predictorCompiledResultTable where updatePriceHistoryStatus <> 1")	;
+$count=db_query("Insert into priceHistoryBatchData select 0,entity_id,BestCouponStatus,NetPrice,ListPrice,CURDATE(),0,BestCouponCode from predictorCompiledResultTable where updatePriceHistoryStatus <> 1")	;
+db_query("update coupon_finder.predictorCompiledResultTable set updatePriceHistoryStatus =0")	;
+db_query("update coupon_finder.1Variables set status = 0 where Serial=3")	;
+}*/
 ?>
