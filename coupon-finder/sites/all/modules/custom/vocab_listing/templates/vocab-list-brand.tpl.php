d03601063a07aca7fea8b2abc6fd52bdcfe1823f
nitin@incaendo.com <nitin@incaendo.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-09-22 08:36:03 +0000

    retail and brand page

--- coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-brand.tpl.php
+++ coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-brand.tpl.php
@@ -6,29 +6,9 @@ foreach ($terms as $term) {
   if (in_array($term->tid, $ignore_tids)) {
     continue;
   }
-  if (in_array(0, $term->parents)) {
-    if ($row == 0) {
-      echo '<div class="vocab-list-parent">';
-      $row++;
-    } else {
-      echo '</div><div class="vocab-list-parent">';
-      $row++;
-    }
-?>
-<h2>
-<?php
-  echo t('%name', array('%name' => $term->name));
-?>
-</h2>
-<?php
-  } else {
 ?>
   <div class="vocab-list-term"><?php echo l($term->name, 'bcp/'.$term->tid.'/coupons-offers'); ?></div>
 <?php
   }
-}
-if ($row != 0) {
-  echo '</div>';
-}
 ?>
 </div>
--- coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-retail.tpl.php
+++ coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-retail.tpl.php
@@ -6,34 +6,14 @@ foreach ($terms as $term) {
   if (in_array($term->tid, $ignore_tids)) {
     continue;
   }
-  if (in_array(0, $term->parents)) {
-    if ($row == 0) {
-      echo '<div class="vocab-list-parent">';
-      $row++;
-    } else {
-      echo '</div><div class="vocab-list-parent">';
-      $row++;
-    }
-?>
-<h2>
-<?php
-  echo t('%name', array('%name' => $term->name));
-?>
-</h2>
-<?php
-  } else {
-    $img = drupal_realpath($term->field_retailer_logo['und'][0]['uri']);
+  $img = image_style_url('retail_logo', $term->field_retailer_logo['und'][0]['uri']);
 ?>
   <div class="vocab-list-term">
     <a href="<?php echo url('rcp/'.$term->name.'/coupons-offers', array('query' => array('field_offer_type_tid' => 'All'))); ?>">
       <img src="<?php echo $img; ?>" alt="<?php echo $term->name; ?>" />
     </a>
   </div>
-<?php
+<?php  
   }
-}
-if ($row != 0) {
-  echo '</div>';
-}
 ?>
 </div>
