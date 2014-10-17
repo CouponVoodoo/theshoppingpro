5a5127f3c8280d3c0a66850f64f4f3723a513564
lavesh626@gmail.com <lavesh626@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-10-17 04:44:17 +0000

    adding domain access

---
+++ coupon-finder/scripts/domain_access_node_script.php
@@ -0,0 +1,58 @@
+<?php
+
+$results = db_select('node', 'n')->fields('n', array('nid'))
+        // run on specific node to test
+		 ->condition('nid', 2476883)
+        ->execute()->fetchAll();
+$nodesProcessed = 0;
+$file = fopen('domain.txt', 'a+');
+foreach ($results as $result) {
+    print "\r\n\r\nNode being processed = $result->nid";
+    fwrite($file, "\r\n\r\nNode being processed = $result->nid");
+    
+    // Check if entry exists in domain_access.
+    $ifDomainExists = db_select('domain_access', 'da')->condition('gid', 3)->condition('realm', 'domain_id')
+            ->condition('nid', $result->nid)->countQuery()->execute()->fetchField();
+
+    if ($ifDomainExists == 0) {
+        // Insert into Domain Access Table
+        db_insert('domain_access')
+                ->fields(array(
+                    'nid' => $result->nid,
+                    'gid' => 3,
+                    'realm' => 'domain_id'
+                ))
+                ->execute();
+        print "\r\nData added for domain_access table for NID = $result->nid";
+        fwrite($file, "\r\nData added for domain_access table for NID = $result->nid");
+    }
+    
+    // Check if entry exists in node_access.
+    $ifNodeExists = db_select('node_access', 'na')->condition('gid', 3)->condition('realm', 'domain_id')
+            ->condition('nid', $result->nid)->countQuery()->execute()->fetchField();
+    
+    if ($ifNodeExists == 0) {
+        // Insert into Node Access Table
+        db_insert('node_access')
+                ->fields(array(
+                    'nid' => $result->nid,
+                    'gid' => 3,
+                    'realm' => 'domain_id',
+                    'grant_view' => 1,
+                    'grant_update' => 1,
+                    'grant_delete' => 1
+                ))
+                ->execute();
+        print "\r\nData added for node_access table for NID = $result->nid";
+        fwrite($file, "\r\nData added for node_access table for NID = $result->nid");
+    }
+	print "\r\n\r\nCurrent Nodes Processed = $nodesProcessed";
+	print "\r\n\r\n";
+    
+    $nodesProcessed++;
+}
+print "\r\n\r\nTotal Nodes Processed = $nodesProcessed";
+print "\r\n\r\n";
+fwrite($file, "\r\n\r\nTotal Nodes Processed = $nodesProcessed");
+fclose($file);
+?>
