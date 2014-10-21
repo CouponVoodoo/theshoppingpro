46b7ea5e083faa4a6293ea0ff3b1bad4f6cdd973
lavesh626@gmail.com <lavesh626@gmail.com@244f5f58-a3fc-5dcf-b316-fcf940ac2bc3>
2014-10-21 11:45:22 +0000

    brand logo

--- coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-brand.tpl.php
+++ coupon-finder/sites/all/modules/custom/vocab_listing/templates/vocab-list-brand.tpl.php
@@ -1,13 +1,13 @@
 <div class="vocab-list vocab-list-retail">
 <?php
-$result = db_query('SELECT Brand,BrandId from coupon_finder.BrandCoupons where brandid in (7515,9975)');
+$result = db_query('SELECT Brand,BrandId from coupon_finder.BrandCoupons where brandid in (7458,7476,7495,7502,7515,7543,7605,7608,7614,7622,7632,7636,7639,7659,7668,7826,7939,7980,8021,8232,8892,9613,10200,10320)');
   // echo 'cxdcadcd';
 foreach ($result as $record) {
 $brandId=  $record->BrandId; 
 $brand=  $record->Brand;
 
   
-  $img = 'http://plugin.theshoppingpro.com/banners/Adidas.jpg';
+  $img = 'http://plugin.theshoppingpro.com/banners/brandLogo/'.$brandId.'.jpg';
 ?>
   <div class="vocab-list-term">
    <a href="<?php echo url('bcp/'.$brandId.'/coupons-offers', array('query' => array('field_offer_type_tid' => 'All'))); ?>">
